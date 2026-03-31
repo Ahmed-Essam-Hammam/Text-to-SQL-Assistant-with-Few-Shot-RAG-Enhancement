e9a10dff3176f4a0f261dfb703db62c48196fa9e
Ahmed Essam Hammam <ahmedessamhamam@gmail.com>
2026-03-31 19:09:36 +0200

    Update embeddings.py

--- rag/embeddings.py
+++ rag/embeddings.py
@@ -1,7 +1,8 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from config import EMBEDDING_MODEL
 
 
 def get_embeddings():
     return HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+        model_name= EMBEDDING_MODEL
     )
