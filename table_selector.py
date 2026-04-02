9ea2a20ac4bd53b313d9d6c3bdb7b080d0040b0d
Ahmed Essam Hammam <ahmedessamhamam@gmail.com>
2026-04-02 18:19:20 +0200

    Add history parameter to get_relevant_tables function

--- table_selector.py
+++ table_selector.py
@@ -2,13 +2,13 @@ from chains import table_selector_chain, safe_invoke
 from database import list_all_tables
 
 
-def get_relevant_tables(question, db_url: str = None):
+def get_relevant_tables(question, db_url: str = None, history=""):
 
     tables = list_all_tables(db_url=db_url)
 
     response = safe_invoke(
         table_selector_chain,
-        {"question": question, "tables": tables},
+        {"question": question, "tables": tables, "history": history},
         fallback="__INJECTION__"
     )
 
