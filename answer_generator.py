9d7fa65d36fdb8b9daee3d1932b39a4f7bdf1ec8
Ahmed Essam Hammam <ahmedessamhamam@gmail.com>
2026-04-02 18:35:48 +0200

    Add history parameter to get_natural_response function

--- answer_generator.py
+++ answer_generator.py
@@ -1,9 +1,10 @@
 from chains import answer_chain
 
 
-def get_natural_response(question, data, is_limited=False):
+def get_natural_response(question, data, history, is_limited=False):
     return answer_chain.invoke({
         "question": question,
         "data": data,
-        "is_limited": str(is_limited)
+        "is_limited": str(is_limited),
+        "history": history or "No previous conversation."
     }).strip()
